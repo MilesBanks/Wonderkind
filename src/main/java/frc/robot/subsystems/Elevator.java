be92c50e1203bfd47698304e41288e77ce6f8354
MilesBanks <Miles.Banks@lkfn.org>
2023-08-31 20:27:50 -0400

    Elevator set up + typo fix

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -4,14 +4,51 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class Elevator extends SubsystemBase {
+
+  private CANSparkMax Elevator = new CANSparkMax(Constants.CAN_ID_Constants.kElevatorMotorID, MotorType.kBrushless);
+  private final RelativeEncoder elevatorEncoder = Elevator.getEncoder();
   /** Creates a new Elevator. */
-  public Elevator() {}
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
+
+  public void elevatorForward(double speed, int position){
+    Elevator.set(speed);
+      if (elevatorEncoder.getPosition() >= position)
+        elevatorStop();
+  }
+
+  public void elevatorForward(double speed){
+    Elevator.set(speed);
+  }
+
+  public void elevatorBackwords(double speed, int position){
+    Elevator.set(-speed);
+      if (elevatorEncoder.getPosition() <= position)
+        elevatorStop();
+  }
+
+  public void elevatorBackwords(double speed){
+
+    Elevator.set(-speed);
+  }
+
+  public void elevatorStop(){
+    Elevator.set(0);
+  }
+
+  public double getEncoder(){
+    return elevatorEncoder.getPosition();
+  }
+
 }
